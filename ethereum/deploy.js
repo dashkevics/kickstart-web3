6dfdd7a8c597f0944f689cf783a3e1c97599e76c
dashkevics <dashkevics@gmail.com>
2022-11-08 14:11:27 +0600

    Complete test file & deploy

---
+++ ethereum/deploy.js
@@ -0,0 +1,27 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const compiledFactory = require('./build/CampaignFactory.json');
+
+const provider = new HDWalletProvider(
+    'sort tail tower uphold grace panel merry sketch when demise margin occur',
+    // remember to change this to your own phrase!
+    'https://goerli.infura.io/v3/e0324e0bda46433b85854698db1432b2'
+    // remember to change this to your own endpoint!
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
+
+    console.log('Attempting to deploy from account', accounts[0]);
+
+    const result = await new web3.eth.Contract(
+        JSON.parse(compiledFactory.interface))
+        .deploy({ data: compiledFactory.bytecode })
+        .send({ gas: '1000000', from: accounts[0] });
+
+    console.log('Contract deployed to', result.options.address);
+    provider.engine.stop();
+};
+deploy();
